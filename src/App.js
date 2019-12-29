65693f24a6a0fbc8fbc29d5ff3c57e5bd728c355
Brennan Glynn <brennanglynn@u.boisestate.edu>
2019-12-29 01:05:31 -0700

    I'm Brennan

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Typography from '@material-ui/core/Typography'
+import Grow from '@material-ui/core/Grow'
 import './App.css'
 
 function App() {
-  console.log('help')
+  const [checked, setChecked] = useState(true)
+  const toggleChecked = () => setChecked(checked => !checked)
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <button onClick={toggleChecked}>Toggle</button>
+        <Grow in={checked} {...(checked ? { timeout: 1000 } : {})}>
+          <Typography variant="h1" component="h2" gutterBottom>
+            Hi, I&apos;m Brennan
+          </Typography>
+        </Grow>
+      </header>
     </div>
   )
 }
